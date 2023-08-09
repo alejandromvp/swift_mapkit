db89ba50cd9286860dee795b449a526a5b9a5879
alejandromvp <alejandro18maestro@gmail.com>
2023-08-09 18:58:06 -0400

    upload files

---
+++ leyendoJson/leyendoJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  leyendoJsonApp.swift
+//  leyendoJson
+//
+//  Created by Alejandro Villagrán Poblete on 19-07-23.
+//
+
+import SwiftUI
+
+@main
+struct leyendoJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
